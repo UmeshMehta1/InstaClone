977f96d60bd80e7d1de1f615ae4dee52bb77c77b
UmeshMehta1 <ukmehta1245@gmail.com>
2024-08-19 06:57:23 +0545

    changes

--- Frontend/src/hooks/useGetAllPost.jsx
+++ Frontend/src/hooks/useGetAllPost.jsx
@@ -4,21 +4,23 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 const useGetAllPost = () => {
   const dispatch = useDispatch();
-  useEffect(() => {
-    const fetchAllPost = async () => {
-      try {
-        const res = await axios.get("http://localhost:3000/api/v1/post/all", {
-          withCredentials: true,
-        });
 
-        if (res.data.success) {
-          console.log(res.data);
-          //   dispatch(setPosts(res.data.posts));
-        }
-      } catch (error) {
-        console.log(error);
+  const fetchAllPost = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/v1/post/all", {
+        withCredentials: true,
+      });
+
+      if (res.data.success) {
+        console.log(res.data);
+        //   dispatch(setPosts(res.data.posts));
       }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
     fetchAllPost();
   }, []);
 };
